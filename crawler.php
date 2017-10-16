6a4c4788ce1f768053cef24194eb42b6e6fba1fa
ruanzetao <ruanzetao@gmail.com>
2017-10-16 23:43:10 +0700

    commit

--- crawler.php
+++ crawler.php
@@ -1,20 +1,17 @@
 <?php
 require "simple_html_dom.php";
 $html=file_get_html("http://zuhaus.vn/zu-design-pc150502.html?page=1");
-// $html=file_get_html("https://vnexpress.net/");
 $ds=$html->find("ul.products li");
-// print_r($ds);
-// echo $html;
-// echo count($ds);
-
 foreach ($ds as $sp) {
 	# code...
-	// echo $sp;
 	$price=$sp->find("span.price span",0);
 	echo $price;
-	$name=$sp->find("h3 a",1)->innertext;
-	echo $name;
-	// echo $name;
-
+	echo "<hr/>";
+	$name=$sp->find("a",3)->innertext;
+	echo $name; 
+	echo "<hr/>";
+	$src=$sp->find("div.product-header a img",0)->src;
+	echo $src;  
+	echo "<hr/>";
 		}
 ?>
