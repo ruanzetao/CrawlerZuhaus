7db8e1335d1efa309500e7e085a5bbfe7446e389
ruanzetao <ruanzetao@gmail.com>
2017-10-17 00:43:21 +0700

    commit

--- crawler.php
+++ crawler.php
@@ -1,8 +1,17 @@
 <?php
 require "simple_html_dom.php";
-$html=file_get_html("http://zuhaus.vn/zu-design-pc150502.html?page=1");
-$ds=$html->find("ul.products li");
-foreach ($ds as $sp) {
+
+// $mang=array();
+
+for($t=1;$t<=3;$t++){
+	getDatabase($t);
+}
+
+function getDatabase($page){
+	$html=file_get_html("http://zuhaus.vn/zu-design-pc150502.html?page=$page");
+	$ds=$html->find("ul.products li");
+
+	foreach ($ds as $sp) {
 	# code...
 	$price=$sp->find("span.price span",0);
 	echo $price;
@@ -13,5 +22,30 @@ foreach ($ds as $sp) {
 	$src=$sp->find("div.product-header a img",0)->src;
 	echo $src;  
 	echo "<hr/>";
-		}
+
+	// array_push($mang,new DataClothe($price,$name,$src));
+	}
+}
+
+
+
+	// echo json_encode($mang);
+
+	// print_r($mang);
+
+class DataClothe{
+	public $price;
+	public $name;
+	public $picture;	
+	function DataClothe($price,$name,$picture){
+		$this->$price=$price;
+		$this->$name=$name;
+		$this->$picture=$picture;
+	}
+
+	function INSERTDB(){
+		$qr="INSERT INTO CLOTHES VALUES($price,$name;$picture)";
+		// Nên kiểm tra $this->url đã có trong db chưa
+	}
+}
 ?>
