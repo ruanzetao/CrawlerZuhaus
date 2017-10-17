defa3442abc774affa16ed8b3735582e67022f91
ruanzetao <ruanzetao@gmail.com>
2017-10-17 19:03:28 +0700

    commit1707

--- crawler.php
+++ crawler.php
@@ -3,11 +3,12 @@ require "simple_html_dom.php";
 
 // $mang=array();
 
-for($t=1;$t<=3;$t++){
+for($t=1;$t<=1;$t++){
 	getDatabase($t);
 }
 
-function getDatabase($page){
+
+	function getDatabase($page){
 	$html=file_get_html("http://zuhaus.vn/zu-design-pc150502.html?page=$page");
 	$ds=$html->find("ul.products li");
 
@@ -19,19 +20,30 @@ function getDatabase($page){
 	$name=$sp->find("a",3)->innertext;
 	echo $name; 
 	echo "<hr/>";
-	$src=$sp->find("div.product-header a img",0)->src;
-	echo $src;  
+	$url=$sp->find("div.product-header a img",0)->src;
+	echo $url;  
 	echo "<hr/>";
 
+	// download hinh
+	// $url = 'http://example.com/image.php';
+	// $img = './download/'.basename($src);
+	// file_put_contents($img, file_get_contents($src));
 	// array_push($mang,new DataClothe($price,$name,$src));
-	}
-}
-
-
-
 	// echo json_encode($mang);
-
 	// print_r($mang);
+
+	// test the download function
+
+	// $url = 'https://www.google.com/abc/files/AwAh20isBECxwscp4JiT';
+	$ch = curl_init('$url');
+	$fp = fopen('./download/'.basename($url), 'wb');
+	curl_setopt($ch, CURLOPT_FILE, $fp);
+	curl_setopt($ch, CURLOPT_HEADER, 0);
+	curl_exec($ch);
+	curl_close($ch);
+	fclose($fp);
+}
+}
 
 class DataClothe{
 	public $price;
