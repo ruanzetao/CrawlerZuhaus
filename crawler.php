c48fef6aa62109a16eca2683e20491633884b9b0
ruanzetao <ruanzetao@gmail.com>
2017-10-17 22:04:21 +0700

    no message

--- crawler.php
+++ crawler.php
@@ -16,33 +16,15 @@ for($t=1;$t<=1;$t++){
 	# code...
 	$price=$sp->find("span.price span",0);
 	echo $price;
-	echo "<hr/>";
 	$name=$sp->find("a",3)->innertext;
 	echo $name; 
-	echo "<hr/>";
-	$url=$sp->find("div.product-header a img",0)->src;
-	echo $url;  
-	echo "<hr/>";
+	$hinh=$sp->find("img",0)->src;
+	echo $hinh;
 
-	// download hinh
-	// $url = 'http://example.com/image.php';
-	// $img = './download/'.basename($src);
-	// file_put_contents($img, file_get_contents($src));
-	// array_push($mang,new DataClothe($price,$name,$src));
-	// echo json_encode($mang);
-	// print_r($mang);
+	$img = './hinh/'. basename($hinh);
+	file_put_contents($img, file_get_contents($hinh));
 
-	// test the download function
-
-	// $url = 'https://www.google.com/abc/files/AwAh20isBECxwscp4JiT';
-	$ch = curl_init('$url');
-	$fp = fopen('./download/'.basename($url), 'wb');
-	curl_setopt($ch, CURLOPT_FILE, $fp);
-	curl_setopt($ch, CURLOPT_HEADER, 0);
-	curl_exec($ch);
-	curl_close($ch);
-	fclose($fp);
-}
+	}	
 }
 
 class DataClothe{
